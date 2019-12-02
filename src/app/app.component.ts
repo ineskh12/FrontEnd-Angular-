1812f6347b21f24b56636e30deede280d52a92fd
ineskh12 <ines.khelifi.1@esprit.tn>
2019-12-03 00:51:31 +0100

    show(search)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private idparam = '';
+
 
   id = 'cfR7_EA1gTU';
+
   playerVars = {
     cc_lang_pref: 'en'
   };
@@ -15,6 +18,7 @@ export class AppComponent {
   private ytEvent;
 
   constructor() {}
+
   onStateChange(event) {
     this.ytEvent = event.data;
   }
@@ -30,4 +34,12 @@ export class AppComponent {
     this.player.pauseVideo();
   }
 
+
+  show(Search) {
+    this.idparam = Search.value.substring(32, Search.value.length);
+    console.log(this.idparam );
+    console.log( Search.value.length );
+    console.log('true' +  Search.value);
+    console.log(this.id );
+  }
 }
