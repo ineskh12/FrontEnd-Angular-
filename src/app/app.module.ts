4d3eb72abf89c83e5c3ff8d2dbab3aa92a2078bb
ineskh12 <ines.khelifi.1@esprit.tn>
2019-12-04 01:02:48 +0100

    4 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgxYoutubePlayerModule } from 'ngx-youtube-player';
+
 import { AppComponent } from './app.component';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
 import {FormsModule} from '@angular/forms';
-import { VedioComponent } from './vedio/vedio.component';
+
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { VideoViewComponent } from './video-view/video-view.component';
 import { HistoryComponent } from './history/history.component';
@@ -13,14 +13,14 @@ import { BookmarksComponent } from './bookmarks/bookmarks.component';
 @NgModule({
   declarations: [
     AppComponent,
-    VedioComponent,
+
     SearchBarComponent,
     VideoViewComponent,
     HistoryComponent,
     BookmarksComponent
   ],
   imports: [
-    BrowserModule, NgxYoutubePlayerModule.forRoot(), FormsModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
@@ -28,6 +28,3 @@ import { BookmarksComponent } from './bookmarks/bookmarks.component';
 export class AppModule {
 
 }
-
-platformBrowserDynamic().bootstrapModule(AppModule);
-
